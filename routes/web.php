5d815f3309ae32e7fd6447ec3972cd278bb0172b
ardilalukita14 <lukitasari623@gmail.com>
2021-02-19 20:29:59 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*** Praktikum 1 - Routing Web Framework Laravel  ***/
+
+Route::get('/', function () {
+    echo "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    echo "NIM : 1941720043 <br>Nama : Ardila Lukita Sari";
+});
+
+Route::get('/articles/{id}', function ($id) {
+    echo "Halaman artikel dengan ID $id";
+});
+
+
